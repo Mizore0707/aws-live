5c2d92c7b3922b22544110b2594b921994baa005
Mizore0707 <ooish-wp22@student.tarc.edu.my>
2025-12-09 12:47:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ct4yqs48ekav.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Ooishaohui2004"
+customdb = "employee"
+custombucket = "ooishaohui-employee"
+customregion = "us-east-1"
+
